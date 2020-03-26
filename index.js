d1a385750dc799645e04f34a2dd266ed9ad4ac87
tplevko <tplevko@redhat.com>
2020-03-26 10:42:56 +0100

    small text change

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-import express from 'express';
-import mongodb from 'mongodb';
-import config from './data';
+import express from "express";
+import mongodb from "mongodb";
+import config from "./data";
 
 const app = express();
 const PORT = 4000;
 const client = mongodb.MongoClient;
 
-client.connect(config.DB, { useNewUrlParser: true }, (err, db) => { 
-    if(err) {
-        console.log('database is not connected')
-    }
-    else {
-        console.log('connected!!')
-    }
+client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log("database is not connected");
+  } else {
+    console.log("connected!!");
+  }
 });
 
-app.get('/', (req, res) => {
-    res.json("I love docker!");
+app.get("/", (req, res) => {
+  res.json("Sample change!");
 });
 
 app.listen(PORT, () => {
-    console.log('Your server is running on PORT:',PORT);
+  console.log("Your server is running on PORT:", PORT);
 });
